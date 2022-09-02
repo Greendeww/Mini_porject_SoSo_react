72411f7de3cc7e55d1d10bc58e269a9126983405
lee123so <100946922+lee123so@users.noreply.github.com>
2022-09-02 18:49:30 +0900

    Header-homecon-update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeContents from "../components/homecon/HomeContents";
+import Header from "../components/layout/Header";
+
+const Home = () => {
+  return (
+    <>
+        <Header/>
+        <HomeContents/>
+    </>
+  );
+};
+
+export default Home;
