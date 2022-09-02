45de9af78c72ce83f8b77ba720a3cfa1d2eb63e8
lee123so <100946922+lee123so@users.noreply.github.com>
2022-09-02 19:10:13 +0900

    i

--- src/components/homecon/HomeContents.jsx
+++ src/components/homecon/HomeContents.jsx
@@ -5,7 +5,9 @@ const HomeContents = () => {
     <Stcontainer>
         <StConBox>
             <StImgBox>
-                <StImg importance="auto" src="" alt="" />
+                <ImageSize>
+                <StImg importance="auto" src="https://i.pinimg.com/564x/d9/c8/25/d9c8256448c3b7c1f8dc190264b1283c.jpg" alt="" />
+                </ImageSize>
                 <StImgBoxLike>❤️</StImgBoxLike>
             </StImgBox>
             <StLikeBox>
@@ -22,24 +24,32 @@ const Stcontainer = styled.div`
 `
 
 const StConBox = styled.div`
-    width: 250px;
-    
+   border-radius: 15px;
+margin-bottom:1rem;
+background-color: white;
+// 줄바꿈 방지
+display:inline-block;
+min-width: 20%;
 `
 const StImgBox = styled.div`
-    width: 100%;
-    height: 300px;
-    background-color: #fff;
-    border-radius: 10px;
-    position: relative;
-    padding: 0px 10px;
-    margin-bottom: 5px;
-`;
+position: relative;
+display: inline-flex;
+width: 100%;
+border-radius: 10px;
+overflow: hidden;
+    
+:hover {
+  cursor:pointer;
+  box-shadow: rgba(0,0,0,0.2) 0 0 10px 3px;
+}
+`
+const ImageSize = styled.div`
+min-width:22vw;
+`
 const StImg = styled.img`
-    border: 0;
-    height: auto;
-    max-width: 100%;
-    vertical-align: middle;
-    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `
 const StImgBoxLike = styled.p`
     position: absolute;
