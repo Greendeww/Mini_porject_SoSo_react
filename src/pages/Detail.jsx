4440e5503d908b2c3ecc729df06c0898e0de7bad
Yunjae <nuguri206@gmail.com>
2022-09-03 13:43:41 +0900

    파일올리기(연습)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../components/layout/Header'
 import DetailPost from '../components/detailPost/DetailPost'
+import Image from '../components/image/Image'
 
 const Detail = () => {
   return (
     <>
       <Header/>
       <DetailPost/>
+      <h1>이미지 연습</h1>
+      <Image/>
     </>
     
   )
